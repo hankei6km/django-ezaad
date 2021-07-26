66286f9f2c4252cf5db47eed0bce8893cff6f311
hankei6km <gh-hankei6km@outlook.jp>
2021-07-26 17:29:01 +0900

    Show username in admin

--- ezaad/admin.py
+++ ezaad/admin.py
@@ -4,8 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import   User
 
 class UserAdmin(BaseUserAdmin):
-    # list_display = ['username', 'is_staff', 'is_active']
-    list_display = ['scim_username', 'is_staff', 'is_active']
+    list_display = ['username', 'scim_username', 'is_staff', 'is_active']
 
 
 admin.site.register(User, UserAdmin)
